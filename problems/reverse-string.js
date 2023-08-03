f33921120561731e2ea36e3f01c01f19c45692bb
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-08-03 13:38:13 -0700

    problem 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,7 @@
 module.exports = function reverseString(string) {
-  // Your code here
+
+  if (typeof string !== 'string') {
+    throw new TypeError ("Invalid name! Must be a string!")
+  } 
+  return string.split('').reverse().join('')
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,22 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const reverseString = require("../problems/reverse-string");
+
+describe ('reverse string function', function () {
+    it ('should return the string reversed', function () {
+        const word = "fun";
+        expect(reverseString(word)).to.equal('nuf')
+    })
+
+    it("should throw an error if string is not type of String", () => {
+        const number = 6;
+        const object = { i: "am", an: "object" };
+        const array = ["i", "am", "an", "array"];
+    
+        expect(() => reverseString(number)).to.throw(TypeError);
+        expect(() => reverseString(object)).to.throw(TypeError);
+        expect(() => reverseString(array)).to.throw(TypeError);
+      });
+
+})
