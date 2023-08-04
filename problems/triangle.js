5c24eb87a5c86872f86dd1b4768e1c9d5894e334
Andrei Vorobev <andrey.vorob1995@gmail.com>
2023-08-03 17:00:00 -0700

    Triangle problem

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,35 @@
 // Your code here
+class Triangle {
+    constructor (side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3
+    }
+
+    getPerimeter() {
+        return this.side1 + this.side2 + this.side3
+    }
+
+    hasValidSideLengths() {
+        return (
+            this.side1 + this.side2 > this.side3 &&
+            this.side1 + this.side3 > this.side2 &&
+            this.side2 + this.side3 > this.side1
+          );
+    }
+
+    validate() {
+        this.isValid = this.hasValidSideLengths()
+    }
+    static getValidTriangles(...triangles) {
+        let array = []
+        triangles.forEach(triangle => {
+            if (triangle.isValid === true) {
+                array.push(triangle)
+            }
+        })
+        return array;
+    }
+}
+
+module.exports = Triangle;
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,101 @@
 // Your code here
+const chai = require("chai")
+const expect = chai.expect
+
+const Triangle = require('../problems/triangle')
+// const Scalene = require('../problems/triangle')
+// const Isosceles = require('../problems/triangle')
+
+
+// describe ('Triangle', function () {
+// it("If instance has side1, side2, and side3", function (){
+//     const triangle = new Triangle(3, 4, 5);
+//     expect(triangle).to.have.property('side1')
+//     expect(triangle).to.have.property('side2')
+//     expect(triangle).to.have.property('side3')
+// })
+
+
+//     it("Should return the perimeter of the Triangle instance", function (){
+//         const triangle = new Triangle(3, 4, 5);
+//         const perimeter = triangle.getPerimeter()
+//         expect((perimeter).to.equal(3 + 4 + 5))
+//     })
+
+//     it("Should return true if the sum of two sides is greater than the remaining side", () => {
+//         expect((triangle1.hasValidSideLengths()).to.be.true)
+//     })
+//     it("Should return false if the sum of two sides is less than the remaining side", () => {
+//         expect((triangle2.hasValidSideLengths()).to.be.false)
+//     })
+
+
+//     it("It should add true to isValid key to the instance", () => {
+//         expect((triangle1.isValid).to.be.true) //to.eql(true)
+//     })
+//     it("It should add false to isValid key to the instance", () => {
+//         expect((triangle1.isValid).to.be.false)
+//     })
+
+
+//     it("Should return the all the instances of the valid triangles", () => {
+//         const triangle3 = new Triangle (15, 20, 25)
+//         const triangle4 = new Triangle (20, 25, 30)
+//         expect(() => Triangle.getValidTriangles([triangle1, triangle3, triangle4]).to.equal([""]))
+//     })
+
+// })
+
+
+describe("Triangle", function () {
+    
+    it("should create a new instance with side lengths set correctly", function () {
+      const triangle1 = new Triangle(10, 15, 20);
+      expect(triangle1).to.have.property("side1");
+      expect(triangle1).to.have.property("side2");
+      expect(triangle1).to.have.property("side3");
+    });
+  
+    it("should calculate the perimeter correctly with getPerimeter()", function () {
+      const triangle1 = new Triangle(10, 15, 20);
+      expect(triangle1.getPerimeter()).to.equal(45);
+    });
+  
+    it("should return true for a valid triangle with hasValidSideLengths()", function () {
+        const triangle1 = new Triangle(10, 15, 20);
+      const isValid = triangle1.hasValidSideLengths();
+      expect(isValid).to.be.true;
+    });
+  
+    it("should return false for an invalid triangle with hasValidSideLengths()", function () {
+      const triangle2 = new Triangle(10, 15, 100);
+      const isValid = triangle2.hasValidSideLengths();
+      expect(isValid).to.be.false;
+    });
+  
+    it("should set the isValid property to true for a valid triangle with validate()", function () {
+        const triangle1 = new Triangle(10, 15, 20);
+        triangle1.validate();
+      expect(triangle1).to.have.property("isValid", true);
+    });
+  
+    it("should set the isValid property to false for an invalid triangle with validate()", function () {
+      const triangle2 = new Triangle(10, 15, 100);
+      triangle2.validate();
+      expect(triangle2).to.have.property("isValid", false);
+    });
+  
+    it("should return an array of valid triangles with getValidTriangles()", function () {
+      const triangle1 = new Triangle(10, 15, 20);
+      const triangle2 = new Triangle(10, 15, 100);
+      const triangle3 = new Triangle(20, 25, 30);
+  
+      triangle1.validate();
+      triangle2.validate();
+      triangle3.validate();
+  
+      const validTriangles = Triangle.getValidTriangles(triangle1, triangle2, triangle3);
+      expect(validTriangles).to.include(triangle1);
+      expect(validTriangles).to.include(triangle3);
+    });
+  });
